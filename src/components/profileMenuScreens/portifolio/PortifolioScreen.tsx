462dc26ca74c984cefda499f2da97776256c5be3
Rafael Vendramini <72450490+rafaelbvc@users.noreply.github.com>
2023-09-28 01:19:48 -0300

    v. 0.3.2 - FIndYourMeal Descriptions

--- src/components/profileMenuScreens/portifolio/PortifolioScreen.tsx
+++ src/components/profileMenuScreens/portifolio/PortifolioScreen.tsx
@@ -16,19 +16,32 @@ const PortifolioScreen = ({ className }: TScreensPropsTypes) => {
       />
       <section className="flex justify-center mx-auto paddingYScreens max-w-[80rem] flex-wrap lg:flex-nowrap">
         <section className="flex flex-col items-center p-2 lg:w-1/2 margins">
-          <h4 className="mb-2 text-right text-mediumGray bounceTitles">
+          <h4 className="mb-2 text-right text-bold bounceTitles">
             Find Your Meal
           </h4>
-          <FindYourMeal className="mb-[2rem] lg:mb-0 animate-pingIMG"/>
+          <FindYourMeal className="mb-[2rem] lg:mb-0 animate-pingIMG" />
         </section>
-        <section className="flex flex-col items-center p-2 margins">
-          <p className="pDescription">
+        <section className="flex flex-col p-2 items-left margins animate-pingText">
+          <p className="text-bold">
             Important: Exclusively this project, for desktops only!
           </p>
-          <br/>
-          <p className="pDescription">Project for NextJs13 certification</p>
-          <br/>
-          <p className="pDescription ">...more projects soon...</p>
+          <br /><br /><br />
+          <p>Restaurant Reservation Application
+            <br /><br />
+            •	NextJs 13<br />
+            •	TypeScript<br />
+            •	Prisma (ORM)<br />
+            •	Tailwindcss<br />
+            •	PostgreSQL<br /> <br />
+            •	Loading and Success States<br />
+            •	Static and Dynamic Routes<br />
+            •	Pages: Error, Not Found<br />
+            •	Server and Client Components<br />
+            •	Authentication<br />
+            •	Middleware, Hashers and Json Web Tokens<br />
+            •	Context API - isAuth<br />
+            •	Availability and Scheduling System<br />
+          </p>
         </section>
       </section>
     </article>
